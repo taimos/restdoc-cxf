a306104f204c26246b151be9d8e2147da7959293
hoegertn <thorsten.hoeger@hoegernet.com>
2012-12-28 18:34:55 +0100

    adding license information

--- src/main/java/org/restdoc/cxf/RestDocFeature.java
+++ src/main/java/org/restdoc/cxf/RestDocFeature.java
@@ -3,6 +3,26 @@
  */
 package org.restdoc.cxf;
 
+/*
+ * #%L
+ * restdoc-cxf
+ * %%
+ * Copyright (C) 2012 RestDoc.org
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 
@@ -24,7 +44,7 @@ import org.slf4j.LoggerFactory;
 /**
  * @author thoeger
  * 
- *         Copyright 2012, Taimos GmbH
+ *         Copyright 2012, restdoc.org
  * 
  */
 public abstract class RestDocFeature extends AbstractFeature {
